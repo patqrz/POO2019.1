96fc281ee85102d2c1aad2fe21d011cf959cc4a3
Patrícia Queiroz <patriciaqueiroz@alu.ufc.br>
2019-04-06 22:09:32 -0300

    subindo projeto carro em JAVA

--- 02_calculadora/calculadora.java
+++ 02_calculadora/calculadora.java
@@ -1,37 +1,38 @@
 import java.util.Scanner;
 
-class Calculadora() {
+class Calculadora {
 	int bateria;
 	int bateriaMax;
 	
 	public Calculadora(int bateria, int bateriaMax) {
-		this.bateria = 0;
+		this.bateria = bateria;
 		this.bateriaMax = bateriaMax;
 	}
 	
-	public void Soma(a, b) {
-		if(this.bateria > 0)
-			this.bateria = this.bateria - 1
-			return a + b;
+	public void soma(double a, double b) {
+		if(this.bateria > 0) {
+			this.bateria = this.bateria - 1;
+			System.out.println(a + b);
 		}else {
 			System.out.println("fail: bateria insuficiente");
 		}
 	}
 	
-	public void Divi(a, b) {
-		if(this.bateria > 0)
-			this.bateria = this.bateria - 1
+	public void divi(double a, double b) {
+		if(this.bateria > 0) {
+			this.bateria = this.bateria - 1;
 			if(b == 0)
                 System.out.println("fail: divisao por 0");
-			return a / b;
+			System.out.println(a / b);
 		}else {
 			System.out.println("fail: bateria insuficiente");
 		}
 	}
 	
-	public void Bateria(bateria, max) {
-		if(m <= this.bateriaMax)
-			this.bateria +=
+	public void recarga(int m) {
+		this.bateria += m;
+		if(this.bateria > this.bateriaMax)
+			this.bateria = bateriaMax;
 	}
 	
 	public String toString() {
@@ -39,8 +40,9 @@ class Calculadora() {
 	}
 }
 
-public class Controle{
+public class Controller{
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 		Calculadora calculadora = new Calculadora(0, 0);
 		
@@ -51,15 +53,18 @@ public class Controle{
 			if(ui[0].equals("end")){
 				break;
 			}else if(ui[0].equals("init")) {
-				calculadora = new Calculadora(Integer.parseInt(ui[1]), Integer.parseInt(ui[2]))
+				calculadora = new Calculadora(Integer.parseInt(ui[1]), Integer.parseInt(ui[2]));
 			}else if(ui[0].equals("show")) {
 				System.out.println(calculadora);
+			}else if(ui[0].equals("charge")) {
+				calculadora.recarga(Integer.parseInt(ui[1]));
 			}else if(ui[0].equals("sum")) {
-				calculadora.Soma();
+				calculadora.soma(Integer.parseInt(ui[1]), Integer.parseInt(ui[2]));
 			}else if(ui[0].equals("div")) {
-				calculadora.Divi();
+				calculadora.divi(Integer.parseInt(ui[1]), Integer.parseInt(ui[2]));
 			}else {
 				System.out.println("fail: comando invalido");
+			}
 		}
 	}
 }
